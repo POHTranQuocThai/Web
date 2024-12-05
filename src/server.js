76e1cd508f40de8a7c77eacf1da62fb6ccf9e0f1
POHTranQuocThai <tranthai.070104@gmail.com>
2024-12-05 10:10:08 +0700

    Kết Nối Tới MySQL Database với Sequelize - Render Dữ Liệu Ra View | Cấu hình cho Node.JS phần 3

--- src/server.js
+++ src/server.js
@@ -2,19 +2,23 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import { configViewEngine } from './config/viewEngine'
 import { initWebRoutes } from './route/web'
-import dotenv from 'dotenv'
-dotenv.config()
+import { connectDB } from './config/connectDB'
+import { env } from './config/environment'
+
 
 
 const app = express()
 //config app
 
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 configViewEngine(app)
 initWebRoutes(app)
 
-const port = process.env.PORT || 6969
+connectDB()
+
+const port = env.PORT || 6969
 app.listen(port, () => {
     console.log('Backend Nodejs is running on the port: localhost:' + port)
 
