d01b97f6975856f33cf8a849e55ec3c309905083
POHTranQuocThai <tranthai.070104@gmail.com>
2024-12-05 09:09:28 +0700

    Tạo tables, thêm dữ liệu database với Sequelize CLI - Cấu hình database cho Node.JS

--- src/route/web.js
+++ src/route/web.js
@@ -1,9 +1,10 @@
 import express from 'express'
+import { homeController } from '../controllers/homeController'
 
 const router = express.Router()
 
 export const initWebRoutes = (app) => {
-    router.get('/', (req, res) => { return res.send('Hello') })
+    router.get('/', homeController.getHomePage)
     return app.use('/', router)
 }
 
