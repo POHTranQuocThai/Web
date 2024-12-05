f35a7e83157ef1ee425005e5aa7d44bc60c3351f
POHTranQuocThai <tranthai.070104@gmail.com>
2024-12-05 13:13:20 +0700

    Tạo Tất Cả Models, Migrations và Seeders Cho Dự Án Đặt Lịch Khám Bệnh

--- src/seeders/seeder-users.cjs
+++ src/seeders/seeder-users.cjs
@@ -4,9 +4,14 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Users', [
       {
-        firstName: 'John',
-        lastName: 'Doe',
-        email: 'example@example.com',
+        password: '12345',
+        firstName: 'Thai',
+        lastName: 'Dev',
+        email: 'thaidev@gmail.com',
+        address: 'VIETNAM',
+        gender: 1,
+        typeRole: 'ROLE',
+        keyRole: 'R1',
         createdAt: new Date(),
         updatedAt: new Date(),
       },
