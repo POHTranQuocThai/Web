00157a5e3097a6b07dc08ec45c803a40e48c00fc
POHTranQuocThai <tranthai.070104@gmail.com>
2024-12-07 09:13:48 +0700

    Hoàn Thiện Chức Năng Đăng Nhập Kết Hợp React.Js Và Node.js

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,12 +3,13 @@ import { userService } from "../services/userService"
 
 const login = async (req, res) => {
     const email = req.body.email
+    console.log('🚀 ~ login ~ email:', email)
     const password = req.body.password
     if (!email || !password) {
-        return res.status(200).json({ message: 'Missing inputs parameter!' })
+        return res.status(404).json({ message: 'Missing inputs parameter!' })
     }
-    const userData = await userService.hanldeUserLogin(email, password)
-    return res.status(200).json(userData.user ? userData : {})
+    const userData = await userService.handleUserLogin(email, password)
+    return res.status(200).json(userData)
 }
 export const userController = {
     login
--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import { configViewEngine } from './config/viewEngine'
 import { initWebRoutes } from './route/web'
 import { connectDB } from './config/connectDB'
 import { env } from './config/environment'
+import cors from 'cors'
 
 
 
@@ -12,6 +13,10 @@ const app = express()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors({
+    origin: 'http://localhost:3000', // Thay bằng domain của bạn
+    credentials: true, // Cho phép gửi cookie hoặc xác thực
+}))
 
 configViewEngine(app)
 initWebRoutes(app)
